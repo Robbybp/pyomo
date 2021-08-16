4cd18954c658b876d2282985a81af1038399d750
Robert Parker <robbybparker@gmail.com>
2021-08-15 22:56:37 -0400

    VectorValuedExternalFunction base class

--- pyomo/contrib/pynumero/interfaces/external_grey_box.py
+++ pyomo/contrib/pynumero/interfaces/external_grey_box.py
@@ -79,6 +79,37 @@ Note:
 
 """
 
+
+class VectorValuedExternalFunction(object):
+    """
+    This is a base class for abstract functions. I differentiate this
+    from an ExternalGreyBoxModel as I believe the "function" and the
+    "model" should be separated, and because I need a different
+    Hessian format.
+
+    """
+
+    def n_inputs(self):
+        raise NotImplementedError()
+
+    def set_input_values(self, input_values):
+        raise NotImplementedError()
+
+    def n_outputs(self):
+        raise NotImplementedError()
+
+    def evaluate_outputs(self):
+        raise NotImplementedError()
+
+    def evaluate_jacobian_outputs(self):
+        # Not all functions have first derivatives
+        pass
+
+    def evaluate_hessian_outputs(self):
+        # Not all functions have second derivatives
+        pass
+
+
 class ExternalGreyBoxModel(object):
     """
     This is the base class for building external input output models
