e25503053cab4833c756102f5aa6141830b8ba4f
Robert <robbybparker@gmail.com>
2021-11-15 23:58:40 -0500

    do not access original NLPs objective unless we want to exclude its Hessian

--- pyomo/contrib/pynumero/interfaces/nlp_projections.py
+++ pyomo/contrib/pynumero/interfaces/nlp_projections.py
@@ -359,8 +359,8 @@ class ProjectedNLP(_BaseNLPDelegator):
     def evaluate_hessian_lag(self, out=None):
         # Cache original NLP's duals and temporarily set those for
         # constraints we don't want to zero.
-        cached_obj_factor = self._original_nlp.get_obj_factor()
         if not self._include_objective:
+            cached_obj_factor = self._original_nlp.get_obj_factor()
             self._original_nlp.set_obj_factor(0.0)
         cached_duals = self._original_nlp.get_duals()
         duals = np.copy(cached_duals)
