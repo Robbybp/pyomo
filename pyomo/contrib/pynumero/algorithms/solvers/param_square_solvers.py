135ee619d15704f0316e7e678bc0d110f567f32c
Robert Parker <robbybparker@gmail.com>
2022-09-19 18:26:03 -0400

    consolidate repeated code and add option for whether or not to use calculate_variable_from_constraint

--- pyomo/contrib/pynumero/algorithms/solvers/param_square_solvers.py
+++ pyomo/contrib/pynumero/algorithms/solvers/param_square_solvers.py
@@ -43,6 +43,7 @@ from pyomo.contrib.incidence_analysis.triangularize import (
     get_projected_directed_graph_from_matching,
     get_scc_dag,
 )
+from pyomo.contrib.incidence_analysis import IncidenceGraphInterface
 from pyomo.contrib.incidence_analysis.util import (
     generate_strongly_connected_components,
 )
@@ -65,7 +66,7 @@ class CyIpoptSolverWrapper(object):
         return self._cyipopt_solver.solve(**kwds)
 
 
-class SccMultiNlpHybridParamSquareSolver(ParameterizedSquareSolver):
+class SquareDecompositionSolver(ParameterizedSquareSolver):
 
     TimeBins = namedtuple(
         "TimeBins",
@@ -91,26 +92,8 @@ class SccMultiNlpHybridParamSquareSolver(ParameterizedSquareSolver):
         timer=None,
         solver_class=None,
         solver_options=None,
+        use_calc_var=True,
     ):
-        """
-        Arguments
-        ---------
-        model: Block
-            Block to be solved
-        param_vars: List of VarData
-            Variables to be treated as parameters
-        variables: List of VarData
-            Variables to be solved for
-        timer: HierarchicalTimer
-            Timer object used for recording
-        solver_class: NLP Solver
-            Any class that can be constructed with an NLP (to be precise,
-            ProjectedExtendedNLP) and implements a solve method that
-            takes x0 as an argument.
-        solver_options: dict
-            Options dict provided to the constructor of the NLP solver
-
-        """
         self._model = model
         self._param_vars = param_vars
         self._param_var_set = ComponentSet(param_vars)
@@ -123,6 +106,10 @@ class SccMultiNlpHybridParamSquareSolver(ParameterizedSquareSolver):
         if solver_options is None:
             solver_options = {}
         self._solver_options = solver_options
+        # Whether we use calculate_variable_from_constraint
+        self._use_calc_var = use_calc_var
+        # Minimum size of a system that will not be solved with calc-var
+        self._calc_var_cutoff = 2 if self._use_calc_var else 1
 
         self._timer.start(self.time_bins.construct)
 
@@ -131,6 +118,7 @@ class SccMultiNlpHybridParamSquareSolver(ParameterizedSquareSolver):
         )
         if variables is None:
             variables = [
+                # TODO: This should be variables in active constraints.
                 v for v in model.component_data_objects(Var)
                 if not v.fixed and not v in self._param_var_set
             ]
@@ -138,49 +126,54 @@ class SccMultiNlpHybridParamSquareSolver(ParameterizedSquareSolver):
         if len(self.variables) != len(self.equations):
             raise RuntimeError()
 
-        self._scc_list = list(generate_strongly_connected_components(
-            self.equations, variables=self.variables
-        ))
-        self._vector_scc_list = [
-            (scc, inputs) for scc, inputs in self._scc_list
-            if len(scc.vars) > 1
+        subsystems = self.partition_system(self.variables, self.equations)
+        # Switch order for compatibility with generate_subsystem_blocks
+        subsystems = [(eqns, vars) for vars, eqns in subsystems]
+        self._subsystem_list = list(generate_subsystem_blocks(subsystems))
+        self._solver_subsystem_list = [
+            # Subsystems that need a solver (rather than calc-var)
+            (block, inputs) for block, inputs in self._subsystem_list
+            if len(block.vars) >= self._calc_var_cutoff
         ]
 
         # Need a dummy objective to create an NLP
-        for scc, inputs in self._vector_scc_list:
-            scc._obj = Objective(expr=0.0)
+        for block, inputs in self._solver_subsystem_list:
+            block._obj = Objective(expr=0.0)
 
             # I need scaling_factor so Pyomo NLPs I create from these blocks
             # don't break when ProjectedNLP calls get_primals_scaling
-            scc.scaling_factor = Suffix(direction=Suffix.EXPORT)
+            block.scaling_factor = Suffix(direction=Suffix.EXPORT)
             # HACK: scaling_factor just needs to be nonempty.
-            scc.scaling_factor[scc._obj] = 1.0
+            block.scaling_factor[block._obj] = 1.0
 
         # These are the "original NLPs" that will be projected
-        self._vector_scc_nlps = [
-            PyomoNLP(scc) for scc, inputs in self._vector_scc_list
+        self._solver_subsystem_nlps = [
+            PyomoNLP(block) for block, inputs in self._solver_subsystem_list
         ]
-        self._vector_scc_var_names = [
-            [var.name for var in scc.vars.values()]
-            for scc, inputs in self._vector_scc_list
+        self._solver_subsystem_var_names = [
+            [var.name for var in block.vars.values()]
+            for block, inputs in self._solver_subsystem_list
         ]
-        self._vector_proj_nlps = [
+        self._solver_proj_nlps = [
             ProjectedExtendedNLP(nlp, names) for nlp, names in
-            zip(self._vector_scc_nlps, self._vector_scc_var_names)
+            zip(self._solver_subsystem_nlps, self._solver_subsystem_var_names)
         ]
 
         # We will solve the ProjectedNLPs rather than the original NLPs
         self._nlp_solvers = [
             self._solver_class(nlp, options=self._solver_options)
-            for nlp in self._vector_proj_nlps
+            for nlp in self._solver_proj_nlps
         ]
-        self._vector_scc_input_coords = [
+        self._solver_subsystem_input_coords = [
             nlp.get_primal_indices(inputs)
-            for nlp, (scc, inputs) in
-            zip(self._vector_scc_nlps, self._vector_scc_list)
+            for nlp, (subsystem, inputs) in
+            zip(self._solver_subsystem_nlps, self._solver_subsystem_list)
         ]
 
         self._timer.stop(self.time_bins.construct)
+
+    def partition_system(self, variables, equations):
+        raise NotImplementedError()
 
     def update_parameters(self, values):
         self._timer.start(self.time_bins.update_parameters)
@@ -190,9 +183,9 @@ class SccMultiNlpHybridParamSquareSolver(ParameterizedSquareSolver):
 
     def solve(self):
         self._timer.start(self.time_bins.solve)
-        vector_scc_idx = 0
-        for block, inputs in self._scc_list:
-            if len(block.vars) == 1:
+        solver_subsystem_idx = 0
+        for block, inputs in self._subsystem_list:
+            if len(block.vars) < self._calc_var_cutoff:
                 self._timer.start(self.time_bins.calc_var)
                 calculate_variable_from_constraint(
                     block.vars[0], block.cons[0]
@@ -204,12 +197,12 @@ class SccMultiNlpHybridParamSquareSolver(ParameterizedSquareSolver):
                 self._timer.start(self.time_bins.calc_var)
                 self._timer.stop(self.time_bins.calc_var)
 
-                nlp = self._vector_scc_nlps[vector_scc_idx]
-                proj_nlp = self._vector_proj_nlps[vector_scc_idx]
-                input_coords = self._vector_scc_input_coords[vector_scc_idx]
+                nlp = self._solver_subsystem_nlps[solver_subsystem_idx]
+                proj_nlp = self._solver_proj_nlps[solver_subsystem_idx]
+                input_coords = self._solver_subsystem_input_coords[solver_subsystem_idx]
 
-                nlp_solver = self._nlp_solvers[vector_scc_idx]
-                _, local_inputs = self._vector_scc_list[vector_scc_idx]
+                nlp_solver = self._nlp_solvers[solver_subsystem_idx]
+                _, local_inputs = self._solver_subsystem_list[solver_subsystem_idx]
 
                 primals = nlp.get_primals()
                 variables = nlp.get_pyomo_variables()
@@ -249,86 +242,24 @@ class SccMultiNlpHybridParamSquareSolver(ParameterizedSquareSolver):
                 for var, val in zip(variables, new_primals):
                     var.set_value(val, skip_validation=True)
 
-                vector_scc_idx += 1
+                solver_subsystem_idx += 1
         self._timer.stop(self.time_bins.solve)
 
 
-class CompressedSccSolver(ParameterizedSquareSolver):
+class SccMultiNlpHybridParamSquareSolver(SquareDecompositionSolver):
 
-    TimeBins = namedtuple(
-        "TimeBins",
-        [
-            "construct",
-            "update_parameters",
-            "solve",
-            "calc_var",
-            "cyipopt",
-        ],
-    )
-    time_bins = TimeBins(
-        *ParameterizedSquareSolver.time_bins,
-        "calc_var",
-        "newton",
-    )
-
-    def __init__(
-        self,
-        model,
-        param_vars,
-        variables=None,
-        timer=None,
-        solver_class=None,
-        solver_options=None,
-    ):
-        """
-        Arguments
-        ---------
-        model: Block
-            Block to be solved
-        param_vars: List of VarData
-            Variables to be treated as parameters
-        variables: List of VarData
-            Variables to be solved for
-
-        """
-        self._model = model
-        self._param_vars = param_vars
-        self._param_var_set = ComponentSet(param_vars)
-        if timer is None:
-            timer = HierarchicalTimer()
-        self._timer = timer
-        if solver_class is None:
-            #solver_class = FsolveNlpSolver
-            solver_class = CyIpoptSolverWrapper
-        self._solver_class = solver_class
-
-        self._timer.start(self.time_bins.construct)
-
-        self.equations = list(
-            model.component_data_objects(Constraint, active=True)
+    def partition_system(self, variables, equations):
+        igraph = IncidenceGraphInterface()
+        var_blocks, con_blocks = igraph.get_diagonal_blocks(
+            variables, equations
         )
-        if variables is None:
-            variables = [
-                v for v in model.component_data_objects(Var)
-                if not v.fixed and not v in self._param_var_set
-            ]
-        self.variables = variables
-        if len(self.variables) != len(self.equations):
-            raise RuntimeError()
+        return list(zip(var_blocks, con_blocks))
 
-        #
-        # Generate subsystems via strongly connected components
-        #
-        #self._subsystem_list = list(generate_strongly_connected_components(
-        #    self.equations, variables=self.variables
-        #))
-        ###
 
-        #
-        # Generate subsystems to solve sequentially.
-        # These are obtained by compressing adjacent, independent SCCs
-        #
-        imat = get_structural_incidence_matrix(self.variables, self.equations)
+class CompressedSccSolver(SquareDecompositionSolver):
+
+    def partition_system(self, variables, equations):
+        imat = get_structural_incidence_matrix(variables, equations)
         M, N = imat.shape
         matching = maximum_matching(imat)
         node_matching = {r: c + M for r, c in matching.items()}
@@ -344,126 +275,11 @@ class CompressedSccSolver(ParameterizedSquareSolver):
             for subset in scc_partition
         ]
         eqn_partition = [
-            [self.equations[i] for i in subset] for subset in partition
+            [equations[i] for i in subset] for subset in partition
         ]
         var_partition = [
-            [self.variables[matching[i]] for i in subset]
+            [variables[matching[i]] for i in subset]
             for subset in partition
         ]
-        subsystems = zip(eqn_partition, var_partition)
-        self._subsystem_list = list(generate_subsystem_blocks(subsystems))
-        ###
-
-        self._vector_subsystem_list = [
-            (block, inputs) for block, inputs in self._subsystem_list
-            if len(block.vars) > 1
-        ]
-
-        # Need a dummy objective to create an NLP
-        for block, inputs in self._vector_subsystem_list:
-            block._obj = Objective(expr=0.0)
-
-            # I need scaling_factor so Pyomo NLPs I create from these blocks
-            # don't break when ProjectedNLP calls get_primals_scaling
-            block.scaling_factor = Suffix(direction=Suffix.EXPORT)
-            # HACK: scaling_factor just needs to be nonempty.
-            block.scaling_factor[block._obj] = 1.0
-
-        # These are the "original NLPs" that will be projected
-        self._vector_subsystem_nlps = [
-            PyomoNLP(scc) for scc, inputs in self._vector_subsystem_list
-        ]
-        self._vector_subsystem_var_names = [
-            [var.name for var in scc.vars.values()]
-            for scc, inputs in self._vector_subsystem_list
-        ]
-        self._vector_proj_nlps = [
-            ProjectedExtendedNLP(nlp, names) for nlp, names in
-            zip(self._vector_subsystem_nlps, self._vector_subsystem_var_names)
-        ]
-
-        # We will solve the ProjectedNLPs rather than the original NLPs
-        self._nlp_solvers = [
-            self._solver_class(nlp)
-            #self._solver_class(
-            #    nlp, options=dict(tol=1e-8, xtol=1e-12)
-            #)
-            for nlp in self._vector_proj_nlps
-        ]
-        self._vector_subsystem_input_coords = [
-            nlp.get_primal_indices(inputs)
-            for nlp, (scc, inputs) in
-            zip(self._vector_subsystem_nlps, self._vector_subsystem_list)
-        ]
-
-        self._timer.stop(self.time_bins.construct)
-
-    def update_parameters(self, values):
-        self._timer.start(self.time_bins.update_parameters)
-        for var, val in zip(self._param_vars, values):
-            var.set_value(val, skip_validation=True)
-        self._timer.stop(self.time_bins.update_parameters)
-
-    def solve(self):
-        self._timer.start(self.time_bins.solve)
-        vector_scc_idx = 0
-        for block, inputs in self._subsystem_list:
-            if len(block.vars) == 1:
-                self._timer.start(self.time_bins.calc_var)
-                calculate_variable_from_constraint(
-                    block.vars[0], block.cons[0]
-                )
-                self._timer.stop(self.time_bins.calc_var)
-            else:
-                # Transfer variable values into the projected NLP, solve,
-                # and extract values.
-                self._timer.start(self.time_bins.calc_var)
-                self._timer.stop(self.time_bins.calc_var)
-
-                nlp = self._vector_subsystem_nlps[vector_scc_idx]
-                proj_nlp = self._vector_proj_nlps[vector_scc_idx]
-                input_coords = self._vector_subsystem_input_coords[vector_scc_idx]
-
-                nlp_solver = self._nlp_solvers[vector_scc_idx]
-                _, local_inputs = self._vector_subsystem_list[vector_scc_idx]
-
-                primals = nlp.get_primals()
-                variables = nlp.get_pyomo_variables()
-
-                # Set values and bounds from inputs to the SCC.
-                # This works because values have been set in the original
-                # pyomo model, either by a previous SCC solve, or from the
-                # "global inputs"
-                for i, var in zip(input_coords, local_inputs):
-                    # Set primals (inputs) in the original NLP
-                    primals[i] = var.value
-                # This affects future evaluations in the ProjectedNLP
-
-                nlp.set_primals(primals)
-
-                x0 = proj_nlp.get_primals()
-
-                self._timer.start(self.time_bins.cyipopt)
-                #sol, _ = nlp_solver.solve(x0=x0)
-                # TODO: Do I need a consistent return value between different
-                # solvers? Don't thing so, as long as primals get updated.
-                nlp_solver.solve(x0=x0)
-                self._timer.stop(self.time_bins.cyipopt)
-
-                # Set primals from solution in projected NLP. This updates
-                # values in the original NLP
-                #proj_nlp.set_primals(sol)
-                #
-                # Values should already be set in the projected NLP...
-
-                # I really only need to set new primals for the variables in
-                # the ProjectedNLP. However, I can only get a list of variables
-                # from the original Pyomo NLP, so here some of the values I'm
-                # setting are redundant.
-                new_primals = nlp.get_primals()
-                assert len(new_primals) == len(variables)
-                for var, val in zip(variables, new_primals):
-                    var.set_value(val, skip_validation=True)
-
-                vector_scc_idx += 1
-        self._timer.stop(self.time_bins.solve)
+        subsystems = zip(var_partition, eqn_partition)
+        return subsystems
