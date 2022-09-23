ca44b05b597e3ebd1ac0469a78ee36898394ec06
Robert Parker <robbybparker@gmail.com>
2022-09-23 16:26:54 -0400

    implement solver that takes secant steps before using Newtons method

--- pyomo/contrib/pynumero/algorithms/solvers/scipy_solvers.py
+++ pyomo/contrib/pynumero/algorithms/solvers/scipy_solvers.py
@@ -138,18 +138,74 @@ class NewtonNlpSolver(DenseSquareNlpSolver):
         domain=float,
         description="Convergence tolerance",
     ))
+    OPTIONS.declare("secant", ConfigValue(
+        default=False,
+        domain=bool,
+        description="Whether to use SciPy's secant method",
+    ))
 
     # TODO: Check that NLP is one-dimensional
     def solve(self, x0=None):
+        self._timer.start("solve")
         if x0 is None:
             x0 = self._nlp.get_primals()
 
+        if self.options.secant:
+            fprime = None
+        else:
+            fprime = lambda x: self.evaluate_jacobian(np.array([x]))[0, 0]
         results = sp.optimize.newton(
             lambda x: self.evaluate_function(np.array([x]))[0],
             x0[0],
-            fprime=lambda x: self.evaluate_jacobian(np.array([x]))[0, 0],
+            fprime=fprime,
             tol=self.options.tol,
         )
+        self._timer.stop("solve")
+        return results
+
+
+class SecantNewtonNlpSolver(DenseSquareNlpSolver):
+
+    OPTIONS = ConfigBlock(
+        description="Options for SciPy newton wrapper",
+    )
+    OPTIONS.declare("tol", ConfigValue(
+        default=1e-8,
+        domain=float,
+        description="Convergence tolerance",
+    ))
+    OPTIONS.declare("secant_iter", ConfigValue(
+        default=2,
+        domain=int,
+        description=(
+            "Number of secant iterations to perform before switching"
+            " to Newton's method."
+        ),
+    ))
+
+    # TODO: Check that NLP is one-dimensional
+    def solve(self, x0=None):
+        self._timer.start("solve")
+        if x0 is None:
+            x0 = self._nlp.get_primals()
+
+        try:
+            results = sp.optimize.newton(
+                lambda x: self.evaluate_function(np.array([x]))[0],
+                x0[0],
+                fprime=None,
+                tol=self.options.tol,
+                maxiter=self.options.secant_iter,
+            )
+        except RuntimeError:
+            x0 = self._nlp.get_primals()
+            results = sp.optimize.newton(
+                lambda x: self.evaluate_function(np.array([x]))[0],
+                x0[0],
+                fprime=lambda x: self.evaluate_jacobian(np.array([x]))[0, 0],
+                tol=self.options.tol,
+            )
+        self._timer.stop("solve")
         return results
 
 
