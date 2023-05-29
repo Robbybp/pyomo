2f3571c3918d6d2a70c8a78c724828670d6087b2
robbybp <robbybparker@gmail.com>
2023-05-28 23:20:42 -0600

    move sparsity structure calculations to symbolic methods where convenient

--- pyomo/contrib/pynumero/interfaces/utils.py
+++ pyomo/contrib/pynumero/interfaces/utils.py
@@ -206,7 +206,62 @@ class CondensedSparseSummation(object):
         return ret
 
 
+class StructurePreservingProduct:
+    def __init__(self, mat1, mat2):
+        mat1 = mat1.tocoo()
+        mat2 = mat2.tocoo()
+
+        self._mat1_ones = coo_matrix(
+            (np.ones(mat1.nnz), (mat1.row, mat1.col)), shape=mat1.shape, dtype=int
+        )
+        self._mat2_ones = coo_matrix(
+            (np.ones(mat2.nnz), (mat2.row, mat2.col)), shape=mat2.shape, dtype=int
+        )
+        self._structural_prod = self._mat1_ones.dot(self._mat2_ones).tocoo()
+        structural_prod = self._structural_prod
+        nrow, ncol = structural_prod.shape
+
+        self._prod_row = structural_prod.row
+        self._prod_col = structural_prod.col
+        self._prod_data = np.zeros(structural_prod.nnz)
+
+        # For all (i, j) that are in the "numeric (i, j) set",
+        # set prod_data[i, j] = numeric_prod_data[i, j]
+        structural_flat_coords = ncol*structural_prod.row + structural_prod.col
+        struct_order = np.argsort(structural_flat_coords)
+
+        self._struct_sorted = structural_flat_coords[struct_order]
+
+        #self._ordered_prod_data = prod_data[struct_order]
+        #ordered_prod_data[mask] = numeric_prod.data[numeric_order]
+
+        ordered_to_original = np.arange(structural_prod.nnz)
+        ordered_to_original[struct_order] = np.arange(structural_prod.nnz)
+        self._ordered_to_original = ordered_to_original
+        #prod_data = ordered_prod_data[ordered_to_original]
+        #return sps.coo_matrix((prod_data, (prod_row, prod_col)), shape=(nrow, ncol))
+
+    def prod(self, mat1, mat2):
+        assert mat1.shape == self._mat1_ones.shape
+        assert mat2.shape == self._mat2_ones.shape
+        numeric_prod = mat1.dot(mat2).tocoo()
+        nrow, ncol = numeric_prod.shape
+        numeric_flat_coords = ncol*numeric_prod.row + numeric_prod.col
+        numeric_order = np.argsort(numeric_flat_coords)
+        numeric_sorted = numeric_flat_coords[numeric_order]
+        mask = np.isin(self._struct_sorted, numeric_sorted)
+        prod_data = np.zeros(self._structural_prod.nnz)
+        prod_data[mask] = numeric_prod.data[numeric_order]
+        prod_data = prod_data[self._ordered_to_original]
+        prod_row = self._prod_row
+        prod_col = self._prod_col
+        return sps.coo_matrix((prod_data, (prod_row, prod_col)), shape=(nrow, ncol))
+
+
 def structure_preserving_product(mat1, mat2):
+    # What in this method can change when the nonzero entries
+    # in mat1 and mat2 change?
+
     # TODO: Performance considerations of converting to COO?
     mat1 = mat1.tocoo()
     mat2 = mat2.tocoo()
@@ -223,23 +278,30 @@ def structure_preserving_product(mat1, mat2):
     assert structural_prod.shape == numeric_prod.shape
     nrow, ncol = structural_prod.shape
 
-    # Get the coordinates in the row-major "flattened array" of the nonzeros
+    prod_row = structural_prod.row
+    prod_col = structural_prod.col
+    prod_data = np.zeros(structural_prod.nnz)
+
+    # For all (i, j) that are in the "numeric (i, j) set",
+    # set prod_data[i, j] = numeric_prod_data[i, j]
     structural_flat_coords = ncol*structural_prod.row + structural_prod.col
     numeric_flat_coords = ncol*numeric_prod.row + numeric_prod.col
 
-    # Get elements that are in the structural nonzeros but not numeric nonzeros
-    mask = ~np.isin(structural_flat_coords, numeric_flat_coords)
-    explicit_zero_flat_coords = structural_flat_coords[mask]
+    struct_order = np.argsort(structural_flat_coords)
+    numeric_order = np.argsort(numeric_flat_coords)
 
-    explicit_zero_row, explicit_zero_col = np.divmod(explicit_zero_flat_coords, ncol)
-    n_explicit_zero = explicit_zero_flat_coords.shape[0]
-    explicit_zero_data = np.zeros(n_explicit_zero, dtype=float)
+    struct_sorted = structural_flat_coords[struct_order]
+    numeric_sorted = numeric_flat_coords[numeric_order]
+    mask = np.isin(struct_sorted, numeric_sorted)
 
-    prod_row = np.concatenate((numeric_prod.row, explicit_zero_row))
-    prod_col = np.concatenate((numeric_prod.col, explicit_zero_col))
-    prod_data = np.concatenate((numeric_prod.data, explicit_zero_data))
-    prod = coo_matrix((prod_data, (prod_row, prod_col)), shape=(nrow, ncol))
-    return prod
+    ordered_prod_data = prod_data[struct_order]
+    ordered_prod_data[mask] = numeric_prod.data[numeric_order]
+
+    ordered_to_original = np.arange(structural_prod.nnz)
+    ordered_to_original[struct_order] = np.arange(structural_prod.nnz)
+    prod_data = ordered_prod_data[ordered_to_original]
+
+    return sps.coo_matrix((prod_data, (prod_row, prod_col)), shape=(nrow, ncol))
 
 
 import numpy as np
@@ -666,7 +728,7 @@ class StructurePreservingSolver:
         return sol
 
     def symbolic_backsolve(self, rhs):
-        self._timer.start("symbolic-backsolve")
+        self._timer.start("backsolve-symbolic")
         rhs_dim, nrhs = rhs.shape
         assert self._dim == rhs_dim
         rhs_coords = np.arange(nrhs)
@@ -683,21 +745,64 @@ class StructurePreservingSolver:
             )
             for i, (sub_row, sub_col, to_extract) in enumerate(self._rhs_sub_coords)
         ]
-        rhs_csc = [sm.tocsc() for sm in rhs_submatrices]
-        self._rhs_masks = [
-            (sm.indptr[rhs_coords] < sm.indptr[rhs_coords + 1])
-            for sm in rhs_csc
-        ]
-        # This array maps column coordinates of the compressed matrix
-        # to column coordinates in the full matrix
-        self._compressed_to_full = [rhs_coords[mask] for mask in self._rhs_masks]
-        self._timer.stop("symbolic-backsolve")
+        sol_submatrices = []
+        self._sum_helpers = []
+        self._prod_helpers = []
+        self._rhs_masks = []
+        self._rhs_compr_maps = []
+        self._expanded_col = []
+        for i in range(self._n_blocks):
+            rhs_terms = [rhs_submatrices[i]]
+            prod_helpers = [
+                StructurePreservingProduct(block, sol_submatrices[j])
+                for k, (j, block) in enumerate(self._incident_blocks[i])
+            ]
+            self._prod_helpers.append(prod_helpers)
+            rhs_terms.extend(
+                #-structure_preserving_product(block, sol_submatrices[j])
+                -prod_helpers[k].prod(block, sol_submatrices[j])
+                for k, (j, block) in enumerate(self._incident_blocks[i])
+            )
+            sum_helper = CondensedSparseSummation(rhs_terms)
+            self._sum_helpers.append(sum_helper)
+            rhs_i = sum_helper.sum(rhs_terms)
+
+            rhs_csc = rhs_i.tocsc()
+            rhs_mask = (rhs_csc.indptr[rhs_coords] < rhs_csc.indptr[rhs_coords + 1])
+            compressed_to_full = rhs_coords[rhs_mask]
+            self._rhs_masks.append(rhs_mask)
+            self._rhs_compr_maps.append(compressed_to_full)
+
+            compr_symbolic_sol = _dense_to_full_sparse(rhs_csc[:, rhs_mask].toarray())
+            # Most likely cause of bug is that that I'm not mapping to and from
+            # the dense matrix properly
+            # Maybe that order of nonzero entries differs between different
+            # products?
+
+            nrow_i = len(self._rblocks[i])
+
+            sol_i_row = compr_symbolic_sol.row
+            sol_i_col = compressed_to_full[compr_symbolic_sol.col]
+            self._expanded_col.append(sol_i_col)
+            sol_i_data = compr_symbolic_sol.data
+            symbolic_sol = sps.coo_matrix(
+                (sol_i_data, (sol_i_row, sol_i_col)),
+                shape=(nrow_i, nrhs),
+            )
+            #else:
+            #    sol_i = sps.coo_matrix((len(self._rblocks[i]), nrhs))
+
+            sol_submatrices.append(symbolic_sol)
+
+        self._timer.stop("backsolve-symbolic")
 
     def numeric_backsolve(self, rhs):
-        self._timer.start("numeric-backsolve")
+        self._timer.start("backsolve-numeric")
         rhs_dim, nrhs = rhs.shape
         assert self._dim == rhs_dim
         rhs_coords = np.arange(nrhs)
+
+        self._timer.start("submatrics")
         rhs_submatrices = [
             sps.coo_matrix(
                 (rhs.data[to_extract], (sub_row, sub_col)),
@@ -705,44 +810,75 @@ class StructurePreservingSolver:
             )
             for i, (sub_row, sub_col, to_extract) in enumerate(self._rhs_sub_coords)
         ]
+        self._timer.stop("submatrics")
 
         # TODO: Any alternative to building up this list sequentially?
         sol_submatrices = []
         for i in range(self._n_blocks):
             rhs_terms = [rhs_submatrices[i]]
             # TODO: Cache sparsity structure for sum/product
+            self._timer.start("product")
             rhs_terms.extend(
-                -structure_preserving_product(block, sol_submatrices[j])
-                for (j, block) in self._incident_blocks[i]
+                -self._prod_helpers[i][k].prod(block, sol_submatrices[j])
+                #-structure_preserving_product(block, sol_submatrices[j])
+                for k, (j, block) in enumerate(self._incident_blocks[i])
             )
-            sum_helper = CondensedSparseSummation(rhs_terms)
+            self._timer.stop("product")
+            self._timer.start("sum")
+            #sum_helper = CondensedSparseSummation(rhs_terms)
+            sum_helper = self._sum_helpers[i]
             # This is the RHS of the "subsystem" for this block
             rhs_i = sum_helper.sum(rhs_terms)
+            # Now we should have rhs_i, with a sparsity pattern that is
+            # completely known a priori
+            self._timer.stop("sum")
 
+            # Each LHS submatrix satisfies the strong Hall property, meaning
+            # that its inverse must be treated as completely dense.
             factor = self._lhs_factors[i]
 
-            if np.any(self._rhs_masks[i]):
+            # This mask can only be computed after the sparsity structure of
+            # rhs_i is determined
+            #rhs_csc = rhs_i.tocsc()
+            #rhs_mask = (rhs_csc.indptr[rhs_coords] < rhs_csc.indptr[rhs_coords + 1])
+            #compressed_to_full = rhs_coords[rhs_mask]
+
+            # At this point we know the sparsity structure of the (local) solution.
+            # It is just the structure of rhs_i, where all non-empty columns
+            # are dense. How to construct this sparsity structure?
+            # rhs_csc[:, rhs_mask] = np.ones(len(rblock[i]), len(compressed_to_full))
+            # This works, but is expensive.
+            # Convert to LIL first? Convert to COO, then augment manually?
+
+            rhs_mask = self._rhs_masks[i]
+            compressed_to_full = self._rhs_compr_maps[i]
+
+            if np.any(rhs_mask):
                 rhs_csc = rhs_i.tocsc()
                 # TODO: Most of this work should be doable in symbolic-backsolve
-                compressed_rhs = sps.hstack(
-                    tuple(
-                        rhs_csc.getcol(j)
-                        for j in range(nrhs)
-                        if self._rhs_masks[i][j]
-                    )
-                )
+                self._timer.start("hstack")
+                compressed_rhs = rhs_csc[:, rhs_mask].toarray()
+                self._timer.stop("hstack")
 
                 # Solve to get a dense matrix
-                sol_i_compressed = factor.solve(compressed_rhs.toarray())
+                self._timer.start("solve")
+                #sol_i_compressed = factor.solve(compressed_rhs.toarray())
+                sol_i_compressed = factor.solve(compressed_rhs)
+                self._timer.stop("solve")
                 # TODO: Should be possible to speed this up
+                self._timer.start("dense-to-sparse")
                 sol_i_compressed = _dense_to_full_sparse(sol_i_compressed)
+                self._timer.stop("dense-to-sparse")
 
                 #
                 # "Expand" by mapping column coordinates back to the "full space"
                 #
+                # TODO: should be able to flatten sol_i_compressed and map it
+                # straight into the coordinates of this matrix
                 sol_i_row = sol_i_compressed.row
                 # TODO: This should be doable in symbolic-backsolve
-                sol_i_col = self._compressed_to_full[i][sol_i_compressed.col]
+                #sol_i_col = compressed_to_full[sol_i_compressed.col]
+                sol_i_col = self._expanded_col[i]
                 sol_i_data = sol_i_compressed.data
                 sol_i = sps.coo_matrix(
                     (sol_i_data, (sol_i_row, sol_i_col)),
@@ -754,21 +890,23 @@ class StructurePreservingSolver:
             sol_submatrices.append(sol_i)
 
         # TODO: Most of this work should be doable in structural-backsolve
+        self._timer.start("vstack")
         perm_sol = sps.vstack(sol_submatrices).tocoo()
+        self._timer.stop("vstack")
 
         nrow = self._dim
         sol_row = self._row_perm_to_orig[perm_sol.row]
         sol_col = perm_sol.col
         sol_data = perm_sol.data
         sol = sps.coo_matrix((sol_data, (sol_row, sol_col)), shape=(nrow, nrhs))
-        self._timer.stop("numeric-backsolve")
+        self._timer.stop("backsolve-numeric")
         return sol
 
 
 def _structure_preserving_solve(matrix, rhs, timer=None):
     solver = StructurePreservingSolver(matrix, timer=timer)
     solver.factorize_numeric(matrix)
-    sol = solver.solve(rhs)
-    #solver.symbolic_backsolve(rhs)
-    #sol = solver.numeric_backsolve(rhs)
+    #sol = solver.solve(rhs)
+    solver.symbolic_backsolve(rhs)
+    sol = solver.numeric_backsolve(rhs)
     return sol
