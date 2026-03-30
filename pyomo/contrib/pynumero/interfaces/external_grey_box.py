ec65979fe5c7d495f917125ffba1435efb316c3a
Robert Parker <robbybparker@gmail.com>
2026-03-30 10:31:38 -0400

    remove unused imports

--- pyomo/contrib/pynumero/interfaces/external_grey_box.py
+++ pyomo/contrib/pynumero/interfaces/external_grey_box.py
@@ -7,23 +7,17 @@
 # software.  This software is distributed under the 3-clause BSD License.
 # ____________________________________________________________________________________
 
-import abc
 import logging
-import numpy as np
-from scipy.sparse import coo_matrix
-from pyomo.common.dependencies import numpy as np
 
 from pyomo.common.deprecation import RenamedClass
 from pyomo.common.log import is_debug_set
 from pyomo.common.timing import ConstructionTimer
-from pyomo.core.base import Var, Set, Constraint, value
-from pyomo.core.base.block import BlockData, Block, declare_custom_block
+from pyomo.core.base import Var, Set
+from pyomo.core.base.block import BlockData, Block
 from pyomo.core.base.global_set import UnindexedComponent_index
 from pyomo.core.base.initializer import Initializer
 from pyomo.core.base.set import UnindexedComponent_set
 from pyomo.core.base.reference import Reference
-
-from ..sparse.block_matrix import BlockMatrix
 
 logger = logging.getLogger('pyomo.contrib.pynumero')
 
